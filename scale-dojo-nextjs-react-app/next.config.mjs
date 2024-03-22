dab506781cc748b9df969f43321bf37642515cbf
maxhuffy <42044782+maxhuffy@users.noreply.github.com>
2024-03-22 14:20:23 -0700

    Redirect index to main page

--- scale-dojo-nextjs-react-app/next.config.mjs
+++ scale-dojo-nextjs-react-app/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/dashboard/main',
+                permanent: true,
+            },
+        ]
+    },
+};
 
 export default nextConfig;
