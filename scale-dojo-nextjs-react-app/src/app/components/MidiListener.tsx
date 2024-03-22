c42474065122a8f848f2e0e5f7d6ca5e676e6727
maxhuffy <42044782+maxhuffy@users.noreply.github.com>
2024-03-22 16:05:04 -0700

    Detect midi input device list changes

--- scale-dojo-nextjs-react-app/src/app/components/MidiListener.tsx
+++ scale-dojo-nextjs-react-app/src/app/components/MidiListener.tsx
@@ -1,13 +1,15 @@
 'use client'
 
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {MIDIVal, MIDIValInput} from "@midival/core"
 import { access } from 'fs';
 
-//let MIDIDevices: string[];
 
 // Received an error the first time I compiled the MIDI object
 const MidiListener: React.FC = () => {
+    const [MIDIDevices, setMIDIDevices] = useState<string[]>([]);
+
+    // Get connected MIDI devices
     useEffect(() => {
         if (MIDIVal) {
             MIDIVal.connect().then((accessObject: any) => {
@@ -15,9 +17,23 @@ const MidiListener: React.FC = () => {
                     console.warn("No MIDI inputs detected")
                     return;
                 }
+                
+                setMIDIDevices([]);
+                accessObject.inputs.forEach( function (element: any) {
+                    setMIDIDevices(prevDevices => [...prevDevices, element.name]);
+                });
+
             });
         }
     }, []);
+
+    // Log the MIDI device options when the items change
+    useEffect(() => {
+        if (MIDIDevices.length != 0)
+            console.log(MIDIDevices);
+    }, [MIDIDevices]);
+
+    
 
     return <div><p>Learning!</p></div>
 }
